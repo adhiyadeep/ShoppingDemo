aba341ac55c26896b4229902c3bdb051bb5dd424
Deep <adhiyadeep@gmail.com>
2018-11-13 10:42:39 +0530

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+
+  slideData = [{ image: "../../assets/imgs/slide1.jpeg" },
+  { image: "../assets/imgs/slide2.jpeg" },
+  { image: "../assets/imgs/slide3.jpeg" },
+  { image: "../assets/imgs/slide6.jpg" },
+  { image: "../assets/imgs/slide5.jpg" },
+]
+
+shoppingData = [
+  { image: "../../assets/imgs/tee.png" , title :"Tshirt"},
+  { image: "../../assets/imgs/img2.jpg", title : "Dressess" },
+  { image: "../assets/imgs/img4.jpg",title : "BashOut" },
+  { image: "../assets/imgs/img3.jpg" ,title : "Acessory"},
+  
+
+]
+
   constructor(public navCtrl: NavController) {
 
   }
